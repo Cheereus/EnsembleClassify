741dbfe2a4b8ea1528e694613263cfd86d3a01b6
Cheereus <fanwei1995@hotmail.com>
2021-02-16 15:41:37 +0800

    updates

--- src/IndicateVector.py
+++ src/IndicateVector.py
@@ -2,40 +2,47 @@ import joblib
 import numpy as np
 from Config import dimension_reduction_methods, cluster_method
 from tqdm import trange
+from Decorator import time_indicator
 
-dataset = 'PBMC'
 
-rel_mats = []
-ind_vectors = []
-y_true = []
-y_true_index = []
+@time_indicator
+def get_indicate_vector(dataset):
 
-# load true labels
-rel_true = joblib.load('rel_mat/' + dataset + '/' + dataset + '_True.pkl')
-n_samples = rel_true.shape[0]
+    rel_mats = []
+    ind_vectors = []
+    y_true = []
+    y_true_index = []
 
-# load relevance matrix
-for dr in dimension_reduction_methods:
-    for cm in cluster_method:
-        print(dr, cm)
-        rel_mats.append(joblib.load('rel_mat/' + dataset + '/' + dataset + dr + cm + '.pkl'))
+    # load true labels
+    rel_true = joblib.load('rel_mat/' + dataset + '/_True.pkl')
+    n_samples = rel_true.shape[0]
 
-y_idx = 0
-# get indicate vector
-for i in trange(n_samples):
-    for j in range(i+1, n_samples):
-        vec = []
-        for rel_mat in rel_mats:
-            vec.append(rel_mat[i, j])
-        ind_vectors.append(vec)
-        y_true.append(rel_true[i, j])
-        y_true_index.append(y_idx)
-        y_idx += 1
-        # print(i, j)
+    # load relevance matrix
+    for dr in dimension_reduction_methods:
+        for cm in cluster_method:
+            rel_mats.append(joblib.load('rel_mat/' + dataset + '/' + dr + cm + '.pkl'))
 
-ind_vectors = np.array(ind_vectors)
-print(ind_vectors.shape)
+    y_idx = 0
+    # get indicate vector
+    for i in trange(n_samples):
+        for j in range(i+1, n_samples):
+            vec = []
+            for rel_mat in rel_mats:
+                vec.append(rel_mat[i, j])
+            ind_vectors.append(vec)
+            y_true.append(rel_true[i, j])
+            y_true_index.append(y_idx)
+            y_idx += 1
+            # print(i, j)
 
-joblib.dump(ind_vectors, 'train_data/' + dataset + '_indicator.pkl')
-joblib.dump(y_true, 'train_data/' + dataset + '_labels.pkl')
-joblib.dump(y_true_index, 'train_data/' + dataset + '_y_index.pkl')
+    ind_vectors = np.array(ind_vectors)
+    print(ind_vectors.shape)
+
+    joblib.dump(ind_vectors, 'train_data/' + dataset + '_indicator.pkl')
+    joblib.dump(y_true, 'train_data/' + dataset + '_labels.pkl')
+    joblib.dump(y_true_index, 'train_data/' + dataset + '_y_index.pkl')
+
+
+if __name__ == '__main__':
+    dataset_name = 'PBMC'
+    get_indicate_vector(dataset_name)
