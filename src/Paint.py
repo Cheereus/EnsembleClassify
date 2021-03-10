6229a4c8bb1cac1abc236fdc7c59d1c25524d7ee
Cheereus <fanwei1995@hotmail.com>
2021-03-10 11:03:26 +0800

    updates

--- src/Paint.py
+++ src/Paint.py
@@ -148,7 +148,7 @@ def draw_scatter_across_datasets(datasets, n_clusters):
 
 
 if __name__ == '__main__':
-    draw_scatter_across_datasets(['PBMC', 'Chu cell type', 'Klein', 'Zeisel'], [6, 7, 4, 9])
+    draw_scatter_across_datasets(['PBMCs', 'Chu cell type', 'Klein', 'Zeisel'], [6, 7, 4, 9])
     # paint_scatter_together('data', 9, '_UMAP_')
     # paint_bars_together(dataset='data', ensemble=[0.8071794121270436, 0.7609870645709287])
     # paint_bars_together(dataset='PBMC', ensemble=[0.8640, 0.7430])
